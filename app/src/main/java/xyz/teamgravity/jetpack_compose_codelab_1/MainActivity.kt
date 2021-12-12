9fb8dfffb19cbc9481b8ff4167e62ebdabea94d3
raheemadamboev <raheemideadev@gmail.com>
2021-12-12 23:10:52 +0500

    persisting state

--- app/src/main/java/xyz/teamgravity/jetpack_compose_codelab_1/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/jetpack_compose_codelab_1/MainActivity.kt
@@ -7,7 +7,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -26,7 +30,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
+    val expanded = rememberSaveable { mutableStateOf(false) }
 
     val extraPadding = if (expanded.value) 48.dp else 0.dp
 
@@ -56,7 +60,7 @@ private fun Greeting(name: String) {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnBoarding) {
         OnBoardingScreen { shouldShowOnBoarding = false }
