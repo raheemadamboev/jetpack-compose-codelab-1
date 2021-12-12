a85f82bc139b7661cc587485651cd7af4cf741a2
raheemadamboev <raheemideadev@gmail.com>
2021-12-12 21:34:15 +0500

    changed the surface of the text and added padding

--- app/src/main/java/xyz/teamgravity/jetpack_compose_codelab_1/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/jetpack_compose_codelab_1/MainActivity.kt
@@ -3,11 +3,13 @@ package xyz.teamgravity.jetpack_compose_codelab_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import xyz.teamgravity.jetpack_compose_codelab_1.ui.theme.Jetpackcomposecodelab1Theme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +17,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpackcomposecodelab1Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
                 }
@@ -26,13 +27,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Jetpackcomposecodelab1Theme {
-        Greeting("Android")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
     }
 }
